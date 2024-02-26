75c3cb380135608dab83a12f44abbbf622617fb3
wedad-gamalelden <wedadgamalelden@gmail.com>
2024-02-26 02:58:14 +0200

    Test changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <>
+   <div className="text-center bg-danger">Hello in Test App</div>
+   </>
+  );
+}
+
+export default App;
